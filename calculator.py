d9d7ee6f8cd66adb178ec6e0ab69c72f9f0c72df
Jack Skiera <skyrajang@gmail.com>
2021-04-06 02:55:36 -0400

    added buttons

--- calculator.py
+++ calculator.py
@@ -1,1 +1,33 @@
 import tkinter as tk
+
+root = tk.Tk()
+
+button_9 = tk.Button(root, text="9")
+button_8 = tk.Button(root, text="8")
+button_7 = tk.Button(root, text="7")
+button_6 = tk.Button(root, text="6")
+button_5 = tk.Button(root, text="5")
+button_4 = tk.Button(root, text="4")
+button_3 = tk.Button(root, text="3")
+button_2 = tk.Button(root, text="2")
+button_1 = tk.Button(root, text="1")
+button_0 = tk.Button(root, text="0")
+
+button_sign = tk.Button(root, text="+/-")
+button_equal = tk.Button(root, text="=")
+
+button_9.grid(row=0, column=0)
+button_8.grid(row=0, column=1)
+button_7.grid(row=0, column=2)
+button_6.grid(row=1, column=0)
+button_5.grid(row=1, column=1)
+button_4.grid(row=1, column=2)
+button_3.grid(row=2, column=0)
+button_2.grid(row=2, column=1)
+button_1.grid(row=2, column=2)
+button_0.grid(row=3, column=1)
+
+button_sign.grid(row=3, column=0)
+button_equal.grid(row=3, column=2)
+
+root.mainloop()
